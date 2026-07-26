b0da2c6f17b3efd9facba238ed0ed79782f00f8d
cesarcaraujo-art <cesar.caraujo@gmail.com>
2026-07-26 12:56:39 -0300

    Update Resend API key and clean up server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // COLE SUA CHAVE DO RESEND AQUI (Substitua a string abaixo pela sua API key iniciada em re_)
-const resend = new Resend('re_dLE81Mao_YF381ZBg37bwcbVprwKzXzoV');
+const resend = new Resend('re_cj2gJuRM_CU1ExMMrwSuzSJUfGfw4mXPy');
 
 // Rota de Health Check / Ping
 app.get('/api/ping', (req, res) => {
